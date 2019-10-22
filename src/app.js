b788b0463d97e48e834835f10b61fb3b5adb8c86
Andrew Garcia <grcwebdev@gmail.com>
2019-10-22 17:47:13 -0400

    mobile menu function complete

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import './scss/style.scss';
+
+let menu = document.getElementById('mobile-nav');
+let icon = document.getElementById('icon');
+let header = document.getElementById('header');
+
+icon.addEventListener('click', mobileClicked);
+
+function mobileClicked () {
+    if(menu.style.display === 'block') {
+        menu.style.display = 'none';
+        header.style.marginBottom = '80px';
+    } else {
+        menu.style.display = 'block';
+        header.style.marginBottom = '0';
+    }
+}
